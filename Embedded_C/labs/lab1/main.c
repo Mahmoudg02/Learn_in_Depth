5a47654f50cf3a3c1fd4e18d5135dbac204eb4dc
Mahmoud <hodhashraf@gmail.com>
2022-10-02 22:45:15 +0200

    delete duplicated

--- Embedded_C/labs/lab1/main.c
+++
@@ -1,9 +0,0 @@
-#include "UART.h"
-
-char buff[100] = "mahmoud ashraf Learn with kerolos" ;
-char const buff2[20]="mahmoud ashraf"  ;
-void main(void)
-{
-	Uart_send_string(buff);
-
-}
--- Embedded_C/labs/lab1/startup.s
+++
@@ -1,7 +0,0 @@
-
-.global reset
-reset:
-		ldr sp,= stack_top
-		bl main
-
-stop: 	b stop
